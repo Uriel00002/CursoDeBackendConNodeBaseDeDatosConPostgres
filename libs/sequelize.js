197973eba3abd2879c1770e157fca616d90e952f
leiru <uriellopezaquino1.0@gmail.com>
2023-06-28 20:38:41 -0600

    Tu primer modelo en Sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,14 +1,15 @@
-const { Sequelize } = require('sequelize');
+const{Sequelize}=require('sequelize');
+const{config}=require('./../config/config');
+const setupModels=require('./../db/models');
+const USER=encodeURIComponent(config.dbUser);
+const PASSWORD=encodeURIComponent(config.dbPassword);
+const URI=`postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-const { config } = require('./../config/config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-
-const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
-  logging: true,
+const sequelize=new Sequelize(URI,{
+  dialect:'postgres',
+  logging:true,
 });
-
-module.exports = sequelize;
+setupModels(sequelize);
+sequelize.sync();
+module.exports=sequelize;
