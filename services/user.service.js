cbcd043313c121efd560a8f8ff1d6f83eff5f176
leiru <uriellopezaquino1.0@gmail.com>
2023-06-28 21:39:59 -0600

    Crear, actualizar y eliminar

--- services/user.service.js
+++ services/user.service.js
@@ -1,33 +1,34 @@
-const boom = require('@hapi/boom');
-
-const { models } = require('./../libs/sequelize');
-
-class UserService {
-  constructor() {}
-
-  async create(data) {
-    return data;
-  }
-
-  async find() {
-    const rta = await models.User.findAll();
+const boom=require('@hapi/boom');
+const{models}=require('./../libs/sequelize');
+class UserService{constructor(){}
+async create(data){
+  const newUser=await
+  models.User.create(data);
+  return newUser;
+}
+async find(){
+  const rta=await
+  models.User.findAll();
+  return rta;
+}
+async findOne(id){
+  const user=await
+  models.User.findByPk(id);
+  if(!user){throw boom.notFound('user not found');
+}
+return user;
+}
+  async update(id,changes){
+    const user=await this.findOne(id);
+    const rta=await user.update(changes);
     return rta;
   }
 
-  async findOne(id) {
-    return { id };
-  }
-
-  async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
-  }
-
-  async delete(id) {
-    return { id };
+  async delete(id){
+    const user=await
+    this.findOne(id);
+    await user.destroy();
+    return{id};
   }
 }
-
-module.exports = UserService;
+module.exports=UserService;
